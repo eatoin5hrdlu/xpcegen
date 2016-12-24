254c9a00faa4958345849d5f97f4f76afb724092
Peter Reintjes <peter.reintjes@ncmls.org>
2016-12-24 01:37:11 -0500

    sample evostat added

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # xpcegen
-Generate an XPCE interface for Aduino devices based upon a declarative description using term_expansion/2
+Generate an XPCE interface for Arduino/Bluetooth devices from a specification via term_expansion/2
+
+An Arduino XPCE interface class can be generated from a specification of the form:
+
+iface( <class>, <parent-class>, [ <type>( <name>, <cmd>, <read/write>) ... ])
+
+The following creates a  "lagoon" class as an instance of the "ebutton" class.
+The class will have six variables and appropriate  get_<name> / put_<name> methods
+
+The "get" method reads the value returned by the Arduino, and
+the "put" method will set the variable inside the Arduino 
+(although read-only variables will not have a "put_" method).
+
+The XPCE variable slots can be read directly without invoking Arduino communication
+and will represent the values obtained during the last communication.
+
+iface( lagoon, ebutton,
+      [ int(       temperature,  t, ro),
+	int(target_temperature, tt, rw),
+	int(              host, v1, rw),
+	int(          inducer1, v2, rw),
+	int(          inducer2, v3, rw),
+	int(          inducer3, v4, rw) ]).
+
+
+The mechanism can be general, but the current implementation makes several assumptions
+about the parent class: Viz.  initialization/update methods will be created and contain
+references to the Bluetooth MAC address, socket, and connect, converse, parse_reply,
+save, and restore methods which must be provided in the parent class.
+
+iface( cellstat, ebutton,
+      [ int(       temperature,  t, ro),
+	int(target_temperature, tt, rw),
+	int(         turbidity,  b, ro),
+	int(  target_turbidity, tb, rw),
+	int(        nutrient,    n, rw)	 ]).
+
+iface( supply, ebutton, [ int( volume,  v, rw)]).
+
+iface( sampler, ebutton, 
+       [int(    cycle,  c, rw),
+	int(  samples,  s, rw),
+	int(  lagoon1, d1, rw),
+	int(  lagoon2, d2, rw),
+	int(  lagoon3, d3, rw),
+	int(  lagoon4, d4, rw),
+	int( cellstat, d5, rw)  ]).
+
--- et.pl
+++ et.pl
@@ -1,14 +1,16 @@
 #!/usr/bin/xpce
-:- use_module(library(time)).
+%:- use_module(library(time)).
 :- use_module(library(pce)).
-:- use_module(library(process)).
-:- use_module(library(charsio)).
-:- use_module(library(helpidx)).
-:- use_module(library(lists)).
-:- use_module(library(ctypes)).
+%:- use_module(library(process)).
+%:- use_module(library(charsio)).
+%:- use_module(library(helpidx)).
+%:- use_module(library(lists)).
+%:- use_module(library(ctypes)).
 
-:- use_module(library(time)).
-:- use_module(library(process)).
+%:- use_module(library(time)).
+%:- use_module(library(process)).
+
+:- delete_file(iface).
 
 % Generate and XPCE interface to communicate with an Arduino device
 %
@@ -24,25 +26,29 @@
 % The parent classes contain:
 % the variables for the socket, temperatureUnits, turbidityUnits, volumeUnits
 % and methods : connect, converse, parse_reply
-:- op(1200,xfx,':->').
+
+:- op(1200,xfx,':->'). % Not in PCE mode, so we must define these operators for reading
 :- op(1200,xfx,':<-').
-:- op(700, xfx,'==').
+:- op(910, xfx,'::').
+
+termout([]).
+termout([T|Ts]) :- format('~q.~n', T), termout(Ts).
 
 % Built-in variables for this class, MAC address and Component Name (ID)
-term_expansion(iface(Type,PType,Vars), List) :-
-        expand_vars(Vars, Declarations, Gets, Puts),
-	flatten([ (:- pce_begin_class(Type, PType)),
+
+term_expansion(iface(Type,PType,Vars), []) :-
+        expand_vars(Vars, Declarations, Methods),
+	flatten([ (:-style_check(-singleton)),
+		  (:- pce_begin_class(Type, PType)),
 		  Declarations,
 		  ( initialise(Self, Label:[name]) :->
 				"Initialise the button and connect to device"::
 			send_super(Self, initialise(Label)),
 		        send(Self, slot, myname, Label),
 			send_super(Self, slot, socket, @nil),
-			( bt_device(Label, Mac),
-			  send_super(Self, slot, mac, Mac),
-			  send_super(Self, connect)
-			  ; true
-			),
+		        bt_device(Label, Mac),
+		        send_super(Self, slot, mac, Mac),
+		        send_super(Self, connect),
 			new(Code2, message(Self, show_conversation)),
 			send(Self, recogniser, click_gesture(right,'',single,Code2))),
 		  ( update(Self) :->
@@ -53,37 +59,38 @@ term_expansion(iface(Type,PType,Vars), List) :-
 			),
 		        get(Self, myname, MyName),
 			format(user_error,'Updated ~s~n', MyName)),
-                  Gets,
-                  Puts,
-		  (:- pce_end_class)], List).
-
+                  Methods,
+		  (:- pce_end_class)], List),
+	          tell(iface),
+                  termout(List),
+                  told,
+                  consult(iface).
 
 expand_vars([],[
 		variable(myname,name,get,"Component ID"),
 		variable(mac,name,get,"Bluetooth MAC Address")
 	       ],[
 		(get_myname(Self1,Value1:name) :<- get(Self1,myname,Value1)),
-		(get_mac(Self2,Value2:name) :<- get(Self2,mac,Value2))
-   	       ],[
 		(put_myname(Self1,Value1:name) :<- send(Self1,slot,myname,Value1)),
+		(get_mac(Self2,Value2:name) :<- get(Self2,mac,Value2)),
 		(put_mac(Self2,Value2:name) :<- send(Self2,slot,mac,Value2))
 	       ]).
 
-expand_vars([V|Vs],[D|Ds],[G|Gs],[P|Ps]) :-
-    expand_var(V, D, G, P),
-    expand_vars(Vs,Ds,Gs,Ps).
+expand_vars([V|Vs],[D|Ds],[M|Ms]) :-
+    expand_var(V, D, M),
+    expand_vars(Vs,Ds,Ms).
 
-expand_var(Var,variable(Name,Type,get,Quoted),Get,Put) :-
+expand_var(Var,variable(Name,Type,get,Quoted),Methods) :-
     Var =.. [Type, Name, Cmd, Mode],
     concat_atom(["""",Name,""""], Quoted),
     concat_atom(['get_',Name],GetFunctor),
-    GetPred = [GetFunctor,Self,Value:Type],
-    Get = (GetPred :-> send(Self,converse,[Cmd]), send_super(Self,parse_reply(Name),Value)),
-    expand_put(Mode,Type,Name,Cmd,Put).
-
-expand_put(ro,    _,    _,   _, true).
-expand_put(rw, Type, Name, Cmd, Put) :-
-    concat_atom(['put_',Name],PutFunctor),
-    PutPred = [PutFunctor,Self,Value:Type],
-    Put = (PutPred :-> send(Self,converse,[Cmd,Value])).
+    GetPred =.. [GetFunctor,Self,Value:Type],
+    Get = (GetPred :-> send(Self,converse,[Cmd]), parse_reply(Name,Value)),
+    ( Mode = ro
+      -> Methods = Get
+      ;  concat_atom(['put_',Name],PutFunctor),
+         PutPred =.. [PutFunctor,Self2,Value2:Type],
+	 Put = (PutPred :-> send(Self2,converse,[Cmd,Value2])),
+	 Methods = [Get,Put]
+    ).
 
